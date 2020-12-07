30af06b22e9f26a7911dc154b4096e287139c286
Adam Taylor <adam.taylor@nottingham.ac.uk>
2020-12-07 15:02:58 +0000

    day3

---
+++ day3.py
@@ -0,0 +1,35 @@
+with open(r"day3_in.txt", "r") as f:
+    puzzle_input = [x.strip() for x in f.readlines()]
+
+
+def traverse(tree_map, across, down):
+    pos = (0, 0)
+    max_y = len(tree_map)
+    encounters = {"#": 0, ".": 0}
+    while pos[1] < max_y:
+        # Get row
+        row = tree_map[pos[1]]
+        # Get current location
+        location = row[pos[0]]
+        # Store location
+        encounters[location] += 1
+        # Move
+        pos = ((pos[0] + across) % len(row), pos[1] + down)
+    print(f"You hit {encounters['#']} Trees!")
+    return encounters["#"]
+
+
+# Part 1
+answer1 = traverse(puzzle_input, 3, 1)
+
+# Part 2
+answer2 = (
+    traverse(puzzle_input, 1, 1)
+    * traverse(puzzle_input, 3, 1)
+    * traverse(puzzle_input, 5, 1)
+    * traverse(puzzle_input, 7, 1)
+    * traverse(puzzle_input, 1, 2)
+)
+
+print(f"Part1 Answer {answer1}")
+print(f"Part2 Answer {answer2}")
